c04aa33b32ddb3620695f4c2ef29a9296bc0e3f9
pakeerswamy2001 <112751462+pakeerswamy2001@users.noreply.github.com>
2022-09-13 22:38:31 +0530

    import java.util.Scanner; class pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=i;j++)             {                 System.out.print(j);             }             System.out.println();         }    } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -8,9 +8,9 @@ class pattern
         n=sc.nextInt();
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=n;j++)
+            for(int j=1;j<=i;j++)
             {
-                System.out.print((char)(i+64)+" ");
+                System.out.print(j);
             }
             System.out.println();
         }
